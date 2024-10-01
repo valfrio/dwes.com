f0ee5d185bae6daf45dcf59a582f54a44f8a5682
valfrio <salvador.jose.martinez.jimenez@gmail.com>
2024-10-01 13:30:01 +0200

    ejercicio 1 y 2, adicion estructura

---
+++ ejercicios/primera_tanda/ejercicio1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+<?php
+    $distancia_sol = 15e8;
+    printf("La disntacia al sol es: %3.0f<br>", $distancia_sol);
+
+    $distancia_pluton_sol = 5.9064e9;
+    echo "La disntacia al sol es: $distancia_pluton_sol<br>";
+
+    $diametro_sol = 13927e6;
+    echo "La disntacia al sol es: $diametro_sol<br>";
+
+
+?>
+    
+</body>
+</html>
---
+++ ejercicios/primera_tanda/ejercicio2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 2</title>
+</head>
+<body>
+<?php
+    
+    $numero_octal = 01735;
+    $numero_hexadecimal = 0x3DD;
+    $numero_binario = 0b1111011101;
+
+    echo "El numero $numero_octal en octal es: ". decoct($numero_octal) . "<br>";
+    echo "El numero $numero_hexadecimal en hexadecimal es: ". dechex($numero_hexadecimal) . "<br>";
+    echo "El numero $numero_binario en binario es: ". decbin($numero_binario) . "<br>";
+    
+?>
+    
+</body>
+</html>
