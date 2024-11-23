5618b8da4cdfab52cb5c574fc312cde83a27b31c
JPradillo <jpradillo7@gmail.com>
2024-11-23 15:21:53 +0100

    8 empezado

---
+++ ejercicios/mantenimiento_estado/ejercicio8/includes_ej8.php
@@ -0,0 +1,18 @@
+<?php
+
+function autenticar($usuario, $contraseña){
+    
+    $usuarios_registrados = [
+        'valfrio'       =>  ['nombre' => 'Salvador Martínez Jimenez', 'password' => password_hash("toolbarlinux57", PASSWORD_DEFAULT)],
+        'jpradillo'     =>  ['nombre' => 'Jorge Pradillo Hintenberger', 'password' => password_hash("marta", PASSWORD_DEFAULT)]
+    ];
+
+    if( array_key_exists($usuario, $usuarios_registrados) ){
+        return password_verify($contraseña, $usuarios_registrados[$usuario]['password']);
+    }
+    else{
+        return false;
+    }
+}
+
+?>
--- ejercicios/mantenimiento_estado/ejercicio8/inicio.php
+++ ejercicios/mantenimiento_estado/ejercicio8/inicio.php
@@ -1,7 +1,27 @@
 <?php
 
-require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/inicio_y_fin.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/dwes.com/includes/inicio_y_fin.php');
 
-inicio_html('Ejercicio 8', ['/styles/for'])
+inicio_html('Ejercicio 8', ['/dwes.com/styles/formulario.css', '/dwes.com/styles/general.css']);
+session_start();
+
+?>
+
+<form action="/ejercicios/mantenimiento_estado/ejercicio8/introducir_comentario.php" method="post">
+    <fieldset>
+        <legend>Inicio de sesión</legend>
+
+        <label for="usuario">usuario</label>
+        <input type="text" name="usuario" id="usuario">
+
+        <label for="contraseña">Contraseña</label>
+        <input type="password" name="contraseña" id="contraseña">
+    </fieldset>
+    <input type="button" value="envio">
+</form>
+
+<?php
+
+fin_html();
 
 ?>
